2f81850c526e2281b829e5219d01044043ab4ec6
malopezp <malopezp@169.254.107.244>
2022-10-04 14:21:57 -0600

    Semana 04

---
+++ src/main/java/com/tienda/controller/IndexController.java
@@ -0,0 +1,29 @@
+package com.tienda.controller;
+
+import com.tienda.dao.ClienteDao;
+import com.tienda.domain.Cliente;
+import java.util.Arrays;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.yaml.snakeyaml.util.ArrayStack;
+
+@Controller
+@Slf4j
+public class IndexController {
+
+    @Autowired
+    private ClienteDao clienteDao;
+
+    @GetMapping("/")
+    public String inicio(Model model) {
+
+        log.info("Arquitectura MVC");
+        var clientes=clienteDao.findAll();
+        model.addAttribute("clientes", clientes);
+        return "index";
+    }
+
+}
