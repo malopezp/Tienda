a93b3f55e19a464e6182b237fb7b2ddf22620aa7
malopezp <malopezp@169.254.107.244>
2022-10-14 22:38:43 -0600

    Semana 05 implementando CRUD y plantillas

--- src/main/java/com/tienda/controller/IndexController.java
+++ src/main/java/com/tienda/controller/IndexController.java
@@ -1,13 +1,14 @@
 package com.tienda.controller;
 
-import com.tienda.dao.ClienteDao;
 import com.tienda.domain.Cliente;
+import com.tienda.service.ClienteService;
 import java.util.Arrays;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.yaml.snakeyaml.util.ArrayStack;
 
 @Controller
@@ -15,15 +16,38 @@ import org.yaml.snakeyaml.util.ArrayStack;
 public class IndexController {
 
     @Autowired
-    private ClienteDao clienteDao;
+    private ClienteService clienteService;
 
     @GetMapping("/")
     public String inicio(Model model) {
-
         log.info("Arquitectura MVC");
-        var clientes=clienteDao.findAll();
+        var clientes = clienteService.getClientes();
         model.addAttribute("clientes", clientes);
         return "index";
+    }
+
+    @GetMapping("/nuevoCliente")
+    public String nuevoCliente(Cliente cliente) {
+        return "modificarCliente";
+    }
+
+    @PostMapping("/guardarCliente")
+    public String guardarCliente(Cliente cliente) {
+        clienteService.save(cliente);
+        return "redirect:/";
+    }
+
+    @GetMapping("/modificarCliente/{idCliente}")
+    public String modificarCliente(Cliente cliente, Model model) {
+        cliente = clienteService.getCliente(cliente);
+        model.addAttribute("cliente", cliente);
+        return "modificarCliente";
+    }
+
+    @GetMapping("/eliminarCliente/{idCliente}")
+    public String elimnarCliente(Cliente cliente) {
+        clienteService.delete(cliente);
+        return "redirect:/";
     }
 
 }
